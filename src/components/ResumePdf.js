d5f2fe6347a5f0f67118a51f76704aaab64aca9f
thejesh1698 <thejesh1698@gmail.com>
2021-07-18 01:26:03 +0530

    resume loader fix

--- src/components/ResumePdf.js
+++ src/components/ResumePdf.js
@@ -4,6 +4,7 @@ import resumePath from "../assets/docs/resume.pdf"
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faChevronLeft, faChevronRight} from "@fortawesome/free-solid-svg-icons";
 import {faTelegramPlane} from "@fortawesome/free-brands-svg-icons";
+import {errorEmojis} from "../assets/jsons/constants";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -16,19 +17,41 @@ const ResumePdf = (props) => {
         setNumPages(numPages);
     }
 
+    const emojiIndex = Math.floor(Math.random() * 10) % 9;
+
+    const initialLoader = <>
+        <div className="resume-pdf-loader">
+            <div className="resume-loader-emoji full-width"
+                 dangerouslySetInnerHTML={{__html: errorEmojis[emojiIndex]}}
+                 style={{fontSize: emojiIndex == 8 ? "5rem" : "8rem"}}/>
+            <div className="full-width">
+                <div className="resume-loader-pulse">
+                    <div className="resume-loader-ring"/>
+                    <div className="resume-loader-ring"/>
+                    <div className="resume-loader-ring"/>
+                    <div className="resume-loader-ring"/>
+                </div>
+            </div>
+            <div className="resume-loader-text full-width">
+                The bits are flowing slowly today!
+            </div>
+        </div>
+    </>;
+
     return (
         <div className="resume-pdf-wrapper">
             <div className="resume-pdf-main">
                 <Document
                     file={resumePath}
                     onLoadSuccess={onDocumentLoadSuccess}
+                    loading={initialLoader}
                 >
                     <Page pageNumber={pageNumber}/>
                 </Document>
                 <button className="download-pdf" onClick={() => {
                     setDownloadClicked(1);
                     const anchor_tag = document.getElementById("pdf-download-doc");
-                    if(anchor_tag){
+                    if (anchor_tag) {
                         anchor_tag.click();
                     }
                     setTimeout(function () {
@@ -38,7 +61,7 @@ const ResumePdf = (props) => {
                 }>Download <FontAwesomeIcon icon={faTelegramPlane} className={downloadClicked ? "clicked" : ""}/>
                     <a style={{display: "none"}} href={resumePath} id='pdf-download-doc' download/>
                 </button>
-                <div className="page-controls">
+                {numPages !== null ? <div className="page-controls">
                     <button type="button" className={pageNumber === 1 ? "disabled" : ""} onClick={() => {
                         if (pageNumber !== 1) {
                             setPageNumber(pageNumber - 1);
@@ -50,9 +73,8 @@ const ResumePdf = (props) => {
                             setPageNumber(pageNumber + 1);
                         }
                     }} aria-label="Pdf next page"><FontAwesomeIcon icon={faChevronRight}/></button>
-                </div>
+                </div> : null}
             </div>
-
         </div>
     );
 }
